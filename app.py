f8297b81315c6af814a8000f42cf705b667c0ed4
edwinlowxh <edwinlowxh@gmail.com>
2022-08-16 23:14:40 +0800

    added flask

--- app.py
+++ app.py
@@ -1,2 +1,24 @@
-print("Hello world")
-print('hello')
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@socketio.on('connect')
+def handle_connect():
+    print('Connected')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Disconnected')
+
+@socketio.on('start')
+def handle_start():
+    print('Start')
+
+@socketio.on('log')
+def handle_log(data):
+    print('Log: ' + data) 
+
+if __name__ == '__main__':
+    socketio.run(app)
